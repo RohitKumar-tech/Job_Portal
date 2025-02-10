95595ac9f11907813b2270f058abdf14de565807
rohitkumar <rohitkumar620200@gmail.com>
2025-02-10 09:17:28 +0530

    Made some Protected Routes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,31 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { Button } from "./ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import { BriefcaseBusiness, BriefcaseBusinessIcon, Heart, PenBox } from "lucide-react";
+import { useEffect, useState } from "react";
+import { SignIn } from "@clerk/clerk-react";
+
 
 const Header = () => {
+
+  const [ showSignIn , setShowSignIn ] = useState(false);
+
+  const [search , setSearch] = useSearchParams();
+
+  useEffect(() => {
+    if(search.get('sign-in')){
+      setShowSignIn(true);
+    }
+  }, [search]);
+
+  // this ensures us that when we click outside the box the box will dissappear
+  const handleOverlayClick = (e) => {
+    if(e.target === e.currentTarget) {
+      setShowSignIn(false);
+      setSearch({});
+    }
+  };
+
   return (
     <>
       <nav className="py-4 flex justify-between items-center ">
@@ -10,15 +33,52 @@ const Header = () => {
           <img src="/logo.png" alt="hirrd" className="h-20" />
         </Link>
 
-        <Button variant="outline">Login</Button>
+        <div className="flex gap-8">
+            <SignedOut>
+              <Button variant="outline" onClick={()=>setShowSignIn(true)}>
+                Login
+              </Button>
+            </SignedOut>
+            <SignedIn>
+              {/* add a condition here */}
+              <Button variant="destructive" className="rounded-full">
+                <PenBox size={20} className="mr-2"/>
+                Post a Job
+                </Button>
+                <Link to="/post-job"></Link>
+              <UserButton appearance={{
+                elements:{
+                  avatarBox:"w-10 h-10",
+                },
+              }}
+              >
 
-      {/* <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn> */}
+                <UserButton.MenuItems>
+                  <UserButton.Link 
+                    label = "My Jobs"
+                    labelIcon = {<BriefcaseBusinessIcon size={15} />}
+                    href="/my-jobs"
+                  />
+                  <UserButton.Link 
+                    label = "Saved Jobs"
+                    labelIcon = {<Heart size={15} />}
+                    href="/saved-jobs"
+                  />
+                </UserButton.MenuItems>
+              </UserButton>
+            </SignedIn>
+        </div>
 
+        {showSignIn && (
+          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+          onClick={handleOverlayClick} 
+          >
+          <SignIn 
+          signUpForceRedirectUrl="/onnoarding"
+          fallbackRedirectUrl="/onboarding"
+          />
+        </div>
+        )}
       </nav>
     </>
   )
