4b93b0b09abf5d80892523641876cee420e4bfc0
rohitkumar <rohitkumar620200@gmail.com>
2025-02-12 22:03:19 +0530

    made the job card

--- src/api/apiJobs.js
+++ src/api/apiJobs.js
@@ -1,9 +1,25 @@
 import supabaseClient from "@/utils/supabase";
 
-export async function getJobs(token) {
+export async function getJobs(token , {location , company_id , searchQuery}) {
     const supabase = await supabaseClient(token);
 
-    let { data, error } = await supabase.from("jobs").select("*");
+    let query = supabase
+    .from("jobs")
+    .select("* , company:companies(name,logo_url), saved: saved_jobs(id)");
+
+    if(location) {
+        query = query.eq("location" , location);
+    }
+
+    if(company_id) {
+        query = query.eq("company_id" , company_id);
+    }
+
+    if(searchQuery) {
+        query = query.ilike("title" , `%${searchQuery}%`);
+    }
+
+    let { data, error } = await query;
 
     if (error) {
         console.error("Error fetching jobs:", error);
@@ -11,4 +27,35 @@ export async function getJobs(token) {
     }
 
     return data;
+}
+
+export async function saveJob(token , { alreadySaved } , saveData) {
+    const supabase = await supabaseClient(token);
+
+    if(alreadySaved) {
+        const { data, error: deleteError } = await supabase
+        .from("saved_jobs")
+        .delete()
+        .eq("job_id",saveData.job_id);
+
+        if (deleteError) {
+            console.error("Error Deleting Saved Job: ",deleteError);
+            return null;
+        }
+
+        return data;
+
+    } else {
+        const { data, error: insertError } = await supabase
+        .from("saved_jobs")
+        .insert([saveData])
+        .select()
+
+        if (insertError) {
+            console.error("Error fetching jobs:", insertError);
+            return null;
+        }
+    
+        return data;
+    }
 }
