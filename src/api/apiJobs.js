add0bbe9baf2b95d00a9d5986b9f2cccd8a082fc
rohitkumar <rohitkumar620200@gmail.com>
2025-02-21 22:28:05 +0530

    Created the post-job page

--- src/api/apiJobs.js
+++ src/api/apiJobs.js
@@ -94,3 +94,19 @@ export async function saveJob(token, { alreadySaved }, saveData) {
 
       return data;
 }
+
+export async function addNewJob(token , _ , jobData) {
+    const supabase = await supabaseClient(token);
+  
+      const { data, error } = await supabase
+        .from("jobs")
+        .insert([jobData])
+        .select();
+  
+      if (error) {
+        console.error("Error Creating Job:", error);
+        return null;
+      }
+
+      return data;
+}
--- src/components/application-card.jsx
+++ src/components/application-card.jsx
@@ -1,5 +1,9 @@
-import { Download } from "lucide-react";
-import { Card, CardHeader, CardTitle } from "./ui/card";
+import { Boxes, BriefcaseBusiness, Download, School } from "lucide-react";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "./ui/card";
+import useFetch from "@/hooks/use-fetch";
+import { updateApplicationStatus } from "@/api/apiApplications";
+import { BarLoader } from "react-spinners";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
 
 const ApplicationCard = ({ application , isCandidate = false }) => {
 
@@ -10,8 +14,20 @@ const ApplicationCard = ({ application , isCandidate = false }) => {
         link.click();
     }
 
+    const { loading: loadingHirirngStatus , fn: fnHiringStatus } = useFetch(
+      updateApplicationStatus,
+      {
+        job_id: application.job_id,
+      }
+    );
+
+    const handleStatusChange = (status) => {
+      fnHiringStatus(status);
+    }
+
   return (
     <Card>
+      {loadingHirirngStatus && <BarLoader width={"100%"} color="#36d7b7" />}
       <CardHeader>
         <CardTitle className="flex justify-between font-bold">
           {isCandidate
@@ -25,12 +41,40 @@ const ApplicationCard = ({ application , isCandidate = false }) => {
         </CardTitle>
       </CardHeader>
 
-      {/* <CardContent>
-        <div>
-            
+      <CardContent className="flex flex-col gap-4 flex-1">
+        <div className="flex flex-col md:flex-row justify-between">
+            <div className="flex gap-2 items-center">
+              <BriefcaseBusiness size={15} /> {application?.experience} years of experience
+            </div>
+            <div className="flex gap-2 items-center">
+              <School size={15} /> {application?.education} 
+            </div>
+            <div className="flex gap-2 items-center">
+              <Boxes size={15} /> Skills : {application?.skills}
+            </div>
         </div>
-      </CardContent> */}
-
+        <hr />
+      </CardContent> 
+      <CardFooter className="flex justify-between">
+            <span> {new Date(application?.created_at).toLocaleString()} </span>
+            {isCandidate ? 
+            <span className="capitalize font-bold">
+              Status: {application?.status} 
+            </span> 
+              : (
+                <Select onValueChange={handleStatusChange} defaultValue={application.status}>
+                  <SelectTrigger className="w-52">
+                      <SelectValue placeholder= "Application Status" />
+                  </SelectTrigger>
+                  <SelectContent>
+                              <SelectItem value="applied">Applied</SelectItem>
+                              <SelectItem value="interviewing">Interviewing</SelectItem>
+                              <SelectItem value="hired">Hired</SelectItem>
+                              <SelectItem value="rejected">Rejected</SelectItem>
+                  </SelectContent>
+              </Select>
+              )}
+      </CardFooter>
     </Card>
   );
 };
